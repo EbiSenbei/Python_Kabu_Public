2e3f36131556a449f5844e605afe73b4b823ff60
EBI-GALLERIA <ebisenbe24@gmail.com>
2022-05-10 22:32:26 +0900

    import_STOCK_JPN_yahoo.pyを修正

--- SRC/Blog/import_STOCK_JPN_yahoo.py
+++ SRC/Blog/import_STOCK_JPN_yahoo.py
@@ -1,37 +1,35 @@
-import datetime
-from dateutil.relativedelta import relativedelta
-import yfinance as yf
+
+# conda install pandas
+# conda install pandas-datareader
+# pip install yfinance --upgrade --no-cache-dir
+# conda install xlrd
+import urllib
+
 import pandas as pd
-import urllib.request
-import pandas_datareader as web
+from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
+import datetime
+import os
+# import fix_yahoo_finance as yf
+import yfinance as yf
 
-from Common.common import Common
-from Common.log import Log
-from Dao import T_KBN_Dao, M_BRAND_Dao,M_BRAND_US_Dao, L_STOCK_DAY_Dao
-from Entity import L_STOCK_DAY_Entity
+# float型への変換チェック
+def isfloat(obj: any):
+    if not obj.isdecimal():
+        try:
+            float(obj)
+            return True
+        except ValueError:
+            return False
+    else:
+        return True
 
-FILE_NAME:str = "import_STOCK_JPN_yahoo"
-log: Log = Log(FILE_NAME)
 if __name__ == '__main__':
     # try:
     print("/*--START-----------------------------------------------------*/")
-
-    # df: pd.core.frame.DataFrame
-    # df = get_nasdaq_symbols()
-    # df.to_csv('nasdaq_symbols.csv')
-    # print(len(df))
-
-    # ------------------------------------------------------------------
-    # 各クラスのコンストラクタ(DB接続)を実行
-    tKbnDao: T_KBN_Dao = T_KBN_Dao.T_KBN_Dao()
-    mBrandDao: M_BRAND_Dao = M_BRAND_Dao.M_BRAND_Dao()
-    mBrandUsDao: M_BRAND_US_Dao = M_BRAND_US_Dao.M_BRAND_US_Dao()
-    lStockDayDao: L_STOCK_DAY_Dao = L_STOCK_DAY_Dao.L_STOCK_DAY_Dao()
-
+    print("/*--日本株の株価取得プログラム*/")
     # 初期値の設定
-    brandCode: str = "0000"
-    start = pd.datetime.today() - relativedelta(months=1)   # 取得したい開始期間
-    # start = "2020/01/01"  # 取得したい開始期間
+    symbolCode: str = "0000"
+    start = datetime.datetime(2010, 1, 1)  # 取得したい開始期間
     end = pd.datetime.today()  # 取得したい終了期間(システム日付)
 
     # 全銘柄情報取得
@@ -42,100 +40,122 @@ if __name__ == '__main__':
     save_name = "data_j.xls"
     urllib.request.urlretrieve(url, save_name)
 
-    inputDf: pd.core.frame.DataFrame
-    inputDf = pd.read_excel('data_j.xls')
+    symbolsDf: pd.core.frame.DataFrame
+    symbolsDf = pd.read_excel('data_j.xls')
 
-    # inputDf: pd.core.frame.DataFrame
-    # inputDf = pd.DataFrame({"コード":["2558"]})
-    df: pd.core.frame.DataFrame
-    error_symbols = []
+    # データ出力フォルダを作成
+    new_path = "data"  # フォルダ名
+    if not os.path.exists(new_path):  # ディレクトリがなかったら
+        os.mkdir(new_path)  # 作成したいフォルダ名を作成
+    outputDf: pd.core.frame.DataFrame
+    outputDf = pd.DataFrame(columns=["銘柄","日付", "始値","高値", "安値", "終値", "終値調整", "売買高", "前日増減額", "前日増減率"])
 
-    for idx in range(len(inputDf)):
-        if 0 <= idx :
-            try:
-                brandCode = str(inputDf.loc[idx, "コード"])
-                # df = web.DataReader(inputDf.loc[idx, "BRAND_CD"], "yahoo", start.strftime("%Y/%m/%d"))
-                df = yf.download(brandCode+'.T', start=start, end=end)
-                # df = web.DataReader(brandCode+'.jp', data_source='stooq', start=start, end=end)
-                # df = web.DataReader(brandCode + '.jp', data_source='stooq')
-                df = df.sort_index(ascending=True)
-
-            except Exception as e:
-                log.error_print(e)
-
-            # # CSV出力
-            # df.to_csv(str(inputDf.loc[idx, "Symbol"]) + '.csv')
+    # 取得した銘柄コード一覧から、株価情報を取得する。
+    for codeIdx in range(len(symbolsDf)):
+        if codeIdx >= 0:
+            # 対象の銘柄コードを設定。
+            symbolCode = str(symbolsDf.loc[codeIdx, "コード"])
 
             # システム日付を取得
             sysDateTime: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-            # 銘柄データを整形
+            # 進捗
             strBuf: str = ""
-            strBuf = " \t (" + str(idx + 1) + "/" + str(len(inputDf)) + ") " + str('{:.2f}'.format(((idx + 1) * 100 / (len(inputDf))))) + '%'
-            strBuf = strBuf + "\t銘柄コード：" + str(brandCode)
-            strBuf = strBuf + " \t件数：" + str(len(df)) + "\t" + sysDateTime
-            log.print(strBuf)
+            strBuf = strBuf + "   (" + str(codeIdx + 1) + "/" + str(len(symbolsDf)) + ") " + str(
+                '{:.2f}'.format(((codeIdx + 1) * 100 / len(symbolsDf)))) + '%\t'
+            strBuf = strBuf + "\t銘柄コード：" + str(symbolCode) + "\t日付：" + sysDateTime
+            print(strBuf)
+
+            # 日別株価を取得
+            try:
+                df = yf.download(symbolCode+'.T', start=start, end=end)
+            except ValueError as err:
+                continue
+            except Exception as err:
+                print("Error:" + str(err))
+                break
 
             for i in range(len(df)):
                 try:
                     # 初期化
-                    stockEn: L_STOCK_DAY_Entity = L_STOCK_DAY_Entity.L_STOCK_DAY_Entity()
+                    if i == 0:
+                        fltADJ_CLOSE_PRICE = 0
+
                     # 値を設定
-                    stockEn.strBRAND_CD = brandCode
+                    strBRAND_CD = symbolCode
                     # 始値
-                    if Common.isfloat(str(df.loc[df.index[i], "Open"])):
-                        stockEn.fltOPEN_PRICE = float(df.loc[df.index[i], "Open"])
+                    if isfloat(str(df.loc[df.index[i], "Open"])):
+                        fltOPEN_PRICE = float(df.loc[df.index[i], "Open"])
                     else:
-                        stockEn.fltOPEN_PRICE = 0.00
+                        fltOPEN_PRICE = 0.00
                     # 高値
-                    if Common.isfloat(str(df.loc[df.index[i], "High"])):
-                        stockEn.fltHIGH_PRICE = float(df.loc[df.index[i], "High"])
+                    if isfloat(str(df.loc[df.index[i], "High"])):
+                        fltHIGH_PRICE = float(df.loc[df.index[i], "High"])
                     else:
-                        stockEn.fltHIGH_PRICE = 0.00
+                        fltHIGH_PRICE = 0.00
                     # 安値
-                    if Common.isfloat(str(df.loc[df.index[i], "Low"])):
-                        stockEn.fltLOW_PRICE = float(df.loc[df.index[i], "Low"])
+                    if isfloat(str(df.loc[df.index[i], "Low"])):
+                        fltLOW_PRICE = float(df.loc[df.index[i], "Low"])
                     else:
-                        stockEn.fltLOW_PRICE = 0.00
+                        fltLOW_PRICE = 0.00
                     # 終値
-                    if Common.isfloat(str(df.loc[df.index[i], "Close"])):
-                        stockEn.fltCLOSE_PRICE = float(df.loc[df.index[i], "Close"])
+                    if isfloat(str(df.loc[df.index[i], "Close"])):
+                        fltCLOSE_PRICE = float(df.loc[df.index[i], "Close"])
                     else:
-                        stockEn.fltCLOSE_PRICE = 0.00
+                        fltCLOSE_PRICE = 0.00
                     # 修正後終値
-                    if Common.isfloat(str(df.loc[df.index[i], "Adj Close"])):
-                        stockEn.fltADJ_CLOSE_PRICE = float(df.loc[df.index[i], "Adj Close"])
+                    if isfloat(str(df.loc[df.index[i], "Adj Close"])):
+                        fltADJ_CLOSE_PRICE_before = fltADJ_CLOSE_PRICE
+                        fltADJ_CLOSE_PRICE = float(df.loc[df.index[i], "Adj Close"])
                     else:
-                        stockEn.fltADJ_CLOSE_PRICE = 0.00
+                        fltADJ_CLOSE_PRICE = 0.00
                     # 売買高
-                    if Common.isfloat(str(df.loc[df.index[i], "Volume"])):
-                        stockEn.fltVOLUME_SU = float(df.loc[df.index[i], "Volume"])
+                    if str(df.loc[df.index[i], "Volume"]).isnumeric():
+                        fltVOLUME_SU = float(df.loc[df.index[i], "Volume"])
                     else:
-                        stockEn.fltVOLUME_SU = 0.00
+                        fltVOLUME_SU = 0.00
 
                     # 前日差額（修正後終値 - 始値）
-                    stockEn.fltDIFF_PRICE = stockEn.fltCLOSE_PRICE - stockEn.fltOPEN_PRICE
+                    fltDIFF_PRICE = fltADJ_CLOSE_PRICE - fltADJ_CLOSE_PRICE_before
 
-                    # 前日増減比率((修正後終値 - 始値)＊100/始値)
-                    if stockEn.fltOPEN_PRICE == 0:
-                        stockEn.fltDIFF_RATE = 0
+                    # 前日増減比率((修正後終値(当日) - 修正後終値(前日))＊100/修正後終値(前日))
+                    if fltADJ_CLOSE_PRICE_before == 0:
+                        fltDIFF_RATE = 0
                     else:
-                        stockEn.fltDIFF_RATE = round(
-                            ((stockEn.fltCLOSE_PRICE - stockEn.fltOPEN_PRICE) * 100 / stockEn.fltOPEN_PRICE), 4)
+                        fltDIFF_RATE = round(
+                            ((fltADJ_CLOSE_PRICE - fltADJ_CLOSE_PRICE_before) * 100 / fltADJ_CLOSE_PRICE_before), 4)
 
-                    # 対象日付をセット（システム年＋取得月日）
-                    stockEn.dateTARGET_DT = datetime.datetime.strptime(str(df.index[i]), '%Y-%m-%d 00:00:00')
+                    # 対象日付をセット
+                    dateTARGET_DT = datetime.datetime.strptime(str(df.index[i]), '%Y-%m-%d 00:00:00')
 
-                    # stockEn.print()
-                    # 取得した値をDBに登録
-                    lStockDayDao.pushL_STOCK_DAY(stockEn, sysDateTime)
-                except KeyError:
+                    # 取得した値をDataframeにセット
+                    outputDf = outputDf.append({
+                        '銘柄': strBRAND_CD
+                        , '日付': dateTARGET_DT
+                        , '始値': fltOPEN_PRICE
+                        , '高値': fltHIGH_PRICE
+                        , '安値': fltLOW_PRICE
+                        , '終値': fltCLOSE_PRICE
+                        , '終値調整': fltADJ_CLOSE_PRICE
+                        , '売買高': fltVOLUME_SU
+                        , '前日増減額': fltDIFF_PRICE
+                        , '前日増減率': fltDIFF_RATE
+                    }, ignore_index=True)
+
+                except KeyError as err:
+                    print("KeyError:" + str(err))
                     pass
-                except ValueError:
+                except ValueError as err:
+                    print("ValueError:" + str(err))
                     pass
-                except Exception as e:
-                    log.error_print(e)
+                except Exception as err:
+                    print("Error:" + str(err))
                     break
 
+        # 取得した株価データをExcelデータに出力
+        if "outputDf" in globals():
+            if len(outputDf) > 0:
+                outputDf.to_csv("data/StockData_" + strBRAND_CD + ".csv",encoding="utf_8_sig")
+                outputDf = outputDf.drop(outputDf.index[0:])
 
     print("/*--FINISH-----------------------------------------------------*/")
